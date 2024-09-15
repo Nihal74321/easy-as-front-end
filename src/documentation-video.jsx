fc498c40f5f94e4255dea8a38070f01304d7616f
Nihal74321 <nihal.dharmaraj@gmail.com>
2024-09-16 10:23:11 +1200

    added video logic

--- src/documentation-video.jsx
+++ src/documentation-video.jsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from 'react';
 import './styles/doc-vid.css'
 import { FaPlay } from "react-icons/fa";
 import { FaPause } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
 
 export default function DocumentVideo() {
+    const [handleVideo, setHandleVideo] = useState();
+
+    useEffect(()=> {
+        if(handleVideo) {
+            document.body.style.overflow = 'hidden';
+            window.scrollTo(0,0);
+        } else if (handleVideo === false){
+            const videoElement = document.getElementById('video');
+            if (videoElement) {
+                videoElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+        return () => {
+            document.body.style.overflow = 'auto';
+        }
+    }, [handleVideo])
     return (
+        <>
         <div className='video-wrappper'>
-            <div className="video-player-full">
+            <div className={`video-player-full ${handleVideo ? "show-video-container" : ""}`}>
                 <div className="header-full">
                     <p className='company-name-full'>Easy As</p>
-                    <IoClose className='close-button'/>
+                    <IoClose className='close-button' onClick={() => setHandleVideo(false)}/>
                 </div>
                 <div className="video-container-full">
 
@@ -20,13 +38,18 @@ export default function DocumentVideo() {
                 <h1 className="intro-header">Not yet convinced?</h1>
                 <h2 className="intro-sub">Take the tour.</h2>
             </div>
-            <div className="video-container">
+            <div className="" id='video'>
                 <div className="container-video">
                     <div className="toggle-video-big">
-                        <FaPlay />
+                        <FaPlay onClick={() => setHandleVideo(true)}/>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div className="table-temp">
+
+        </div>
+        </>
     )
 }
